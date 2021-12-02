f49534480a184c6de888a04bdbed11503aa8cc50
Rob G <crx8490@gmail.com>
2021-12-02 10:35:34 +0000

    Add function to convert a list of strings to ints

--- src/Utils.kt
+++ src/Utils.kt
@@ -11,3 +11,8 @@ fun readInput(name: String) = File("src", "$name.txt").readLines()
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
+
+/**
+ * Converts list of strings to list of ints
+ */
+fun List<String>.toIntList() = this.map { it.toInt() }
