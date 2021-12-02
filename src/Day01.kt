1702bf0b3b0ecc1d5562e50c678cd030989e8359
Rob G <crx8490@gmail.com>
2021-12-02 09:59:33 +0000

    Day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,11 +7,31 @@ fun main() {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    // Part 1
+    var increases = 0
+    var i = 0
+
+    while (i < part1(input) - 1) {
+        if (input[i].toInt() < input[i + 1].toInt()) {
+            increases++
+        }
+        i++
+    }
+    println("Part One: $increases")
+
+    // Part 2
+    increases = 0
+    i = 0
+
+    while (i < part2(input) - 3 ) {
+        val sum1 = input[i].toInt() + input[i + 1].toInt() + input[i + 2].toInt()
+        val sum2 = input[i + 1].toInt() + input[i + 2].toInt() + input [i + 3].toInt()
+            if (sum2 > sum1) {
+                increases++
+            }
+        i++
+        }
+    println("Part Two: $increases")
 }
